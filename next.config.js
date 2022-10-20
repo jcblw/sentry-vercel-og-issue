f6081a8914eb53709f331d480d1805fc94d00faa
Jacob Lowe <jcblw@192.168.1.4>
2022-10-20 13:26:10 -0500

    add initial files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const { withSentryConfig } = require("@sentry/nextjs");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sentry: {
+    autoInstrumentServerFunctions: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = withSentryConfig(nextConfig, {});
